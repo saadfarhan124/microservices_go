8fd57fe59cecbb5206093314cd38744f1111a007
Saad Farhan <56558687+saadfarhan124@users.noreply.github.com>
2022-11-03 16:28:47 +0500

    initial commit

---
+++ services/web_blog/database/database.go
@@ -0,0 +1,5 @@
+package database
+
+func Hello() string {
+	return "Hello"
+}
---
+++ services/web_blog/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World")
+	})
+	log.Fatal(app.Listen(":3069"))
+}
