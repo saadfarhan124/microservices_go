aaac53b59a3b75390c79c2017918fd7f24c95a33
saad farhan <saadfarhan124@gmail.com>
2023-02-13 21:02:06 +0500

    part 5

--- services/web_blog/routes/author_routes.go
+++ services/web_blog/routes/author_routes.go
@@ -54,17 +54,27 @@ func GetAllAuthors(c *fiber.Ctx) error {
 }
 
 func GetSingleAuthor(c *fiber.Ctx) error {
+	// Get ID from Params and convert it into int
+	// The second variable is the err value which we have ignored for now
 	id, _ := strconv.Atoi(c.Params("id"))
+
+	// We fetch our base response object
 	response := utilities.GetBaseResponseObject()
+	// Create an author object using the id we get from url params
 	author := &models.Authors{Id: id}
+	// Read the author data and check for errors
 	if err := database.Database.Orm.Read(author); err != nil {
 		response["error"] = err.Error()
+		// Check if the error is of type ErrNoRows
+		// which basically means that no record was found
 		if errors.Is(err, orm.ErrNoRows) {
 			response["message"] = "Author not found"
 			return c.Status(fiber.StatusNotFound).JSON(response)
 		}
+		// If the error is of any other type then return 500 code
 		return c.Status(fiber.StatusInternalServerError).JSON(response)
 	} else {
+		// Return author's data if found
 		response["data"] = author
 		response["status"] = "pass"
 		delete(response, "message")
@@ -73,12 +83,22 @@ func GetSingleAuthor(c *fiber.Ctx) error {
 }
 
 func DeleteAuthor(c *fiber.Ctx) error {
+	// Get base response object
 	response := utilities.GetBaseResponseObject()
+	// Initilize postbody variable for delete author
 	postBody := &validators.AuthorDeletePostBody{}
+	// Use our post body validation function to validate postbody
+	// Return 400 if invalid json or postbody
 	if err := utilities.PostBodyValidation(c, postBody); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(err)
+		return c.Status(fiber.StatusBadRequest).JSON(err)
 	} else {
+		// Create author object using ID
 		author := &models.Authors{Id: postBody.Id}
+		// Use delete method of orm layer that either returns
+		// The number of rows affected
+		// or an error
+		// We can use the number of rows var to cater
+		// record not found and send the apt code back
 		if num, err := database.Database.Orm.Delete(author); err != nil {
 			response["error"] = err.Error()
 			return c.Status(fiber.StatusInternalServerError).JSON(response)
@@ -96,12 +116,22 @@ func DeleteAuthor(c *fiber.Ctx) error {
 }
 
 func UpdateAuthor(c *fiber.Ctx) error {
+	// Get base response object
 	response := utilities.GetBaseResponseObject()
+	// Initilize postbody variable for update author
 	postBody := &validators.AuthorUpdatePostBody{}
+	// Use our post body validation function to validate postbody
+	// Return 400 if invalid json or postbody
 	if err := utilities.PostBodyValidation(c, postBody); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(err)
 	} else {
+		// Create author object using ID and title from postbody
 		author := &models.Authors{Id: postBody.Id, Title: postBody.Title}
+		// Use update method of orm layer that either returns
+		// The number of rows affected
+		// or an error
+		// We can use the number of rows var to cater
+		// record not found and send the apt code back
 		if num, err := database.Database.Orm.Update(author); err != nil {
 			response["error"] = err.Error()
 			return c.Status(fiber.StatusInternalServerError).JSON(response)
--- services/web_blog/utilities/utilities.go
+++ services/web_blog/utilities/utilities.go
@@ -13,14 +13,23 @@ func GetBaseResponseObject() map[string]interface{} {
 }
 
 func PostBodyValidation(c *fiber.Ctx, postBody interface{}) interface{} {
+	// Get fiber context and post body interface as an arguement
+	// Return an interface back
+	// We start by getting the base response object
 	response := GetBaseResponseObject()
+	// Validate if it is a valid json
+	// if not then add error key and send back
 	if err := c.BodyParser(postBody); err != nil {
 		response["error"] = err.Error()
 		return response
 	} else {
+		// check if post body follows the validations accurately
+		// if not then send the errors back
 		if errors := validators.ValidateStruct(postBody); errors != nil {
 			return errors
 		} else {
+			// return nil if postbody is both a valid json
+			// and passes all validation
 			return nil
 		}
 	}
